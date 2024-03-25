62b3f536af3f71e32add4d5fce535f10ed792591
Kypoi <118321745+Kypoi@users.noreply.github.com>
2024-03-25 22:15:27 +0000

    Developed Admin Services and Repo

--- data/src/main/java/proj2/projeto/entities/Admin/AdminRepository.java
+++ data/src/main/java/proj2/projeto/entities/Admin/AdminRepository.java
@@ -1,7 +1,15 @@
 package proj2.projeto.entities.Admin;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import proj2.projeto.entities.Cliente.Cliente;
 
+import java.util.Optional;
+@Repository
 public interface AdminRepository extends JpaRepository<Admin,Long> {
-
+  Admin findById(Integer id);
+  Optional<Admin> findByEmailAndPassword(String email,String password);
+  @Query("SELECT a FROM Admin a WHERE a.email = ?1")
+  Optional<Admin> findByEmail(String email);
 }
