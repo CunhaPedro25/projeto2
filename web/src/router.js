c52fb3dddc28301604efe373da7297909150d09e
Pedro Cunha <pedro10cunha@gmail.com>
2024-06-12 02:34:55 +0100

    feat(web): Added some useful stuff

---
+++ web/src/router.js
@@ -0,0 +1,14 @@
+import { createMemoryHistory, createRouter } from 'vue-router'
+
+import Dashboard from './views/Dashboard.vue'
+
+const routes = [
+    { path: '/', component: Dashboard },
+]
+
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes,
+})
+
+export default router
