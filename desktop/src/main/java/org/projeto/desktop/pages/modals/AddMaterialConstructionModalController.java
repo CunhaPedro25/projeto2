5dd4fe7aa4e313c1b8fe2b8d359e470cc6397d7a
Miguel Liquito <118321745+Kypoi@users.noreply.github.com>
2024-06-16 03:00:34 +0100

    fix(Desktop): now stock updates properly

--- desktop/src/main/java/org/projeto/desktop/pages/modals/AddMaterialConstructionModalController.java
+++ desktop/src/main/java/org/projeto/desktop/pages/modals/AddMaterialConstructionModalController.java
@@ -1,15 +1,20 @@
 package org.projeto.desktop.pages.modals;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import org.projeto.data.entities.Construction;
 import org.projeto.data.entities.ConstructionMaterial;
+import org.projeto.data.entities.ConstructionMaterialId;
+import org.projeto.data.entities.Material;
 import org.projeto.data.services.ConstructionMaterialService;
 import org.projeto.data.services.ConstructionService;
 import org.projeto.data.services.MaterialService;
 import org.projeto.desktop.SceneManager;
 import org.projeto.desktop.components.MaterialConstructionFormController;
+
+import java.util.List;
 
 public class AddMaterialConstructionModalController {
     @FXML
@@ -17,7 +22,7 @@ public class AddMaterialConstructionModalController {
     @FXML
     MaterialConstructionFormController materialConstructionFormController;
 
-    Construction selectedConstruction;
+    Integer constructionID;
     @FXML
     public void initialize() {
         getReady();
@@ -37,13 +42,32 @@ public class AddMaterialConstructionModalController {
                 SceneManager.openErrorAlert("Error edit", "Please fill all the required fields correctly");
             } else {
                 System.out.println("form is correct");
+                if (Integer.parseInt(materialConstructionFormController.quantity.getText()) > MaterialService.getMaterialByName(materialConstructionFormController.materialComboBox.getValue()).getQuantity()){
+                    SceneManager.openErrorAlert("Error", "Not enough material in stock");
+                    return;
+                }
+                Construction test = ConstructionService.findById(constructionID);
+                if (test == null){
+                    System.out.println("Construction is null");
+                    throw new IllegalArgumentException("OLHA O ERRO AQUI!!!!!!!!!!!!!!!!!!!");
+                }
+
                 ConstructionMaterialService.addNew(ConstructionMaterial.builder()
-                        .construction(ConstructionService.findById(Math.toIntExact(Long.parseLong(materialConstructionFormController.constructionID.getText()))))
+                        .id(ConstructionMaterialId.builder()
+                                .construction(constructionID)
+                                .material(MaterialService.getMaterialByName(materialConstructionFormController.materialComboBox.getValue()).getId())
+                                .build())
+                        .construction(test)
                         .material(MaterialService.getMaterialByName(materialConstructionFormController.materialComboBox.getValue()))
                         .quantity(Integer.valueOf(materialConstructionFormController.quantity.getText()))
                         .build());
 
                 SceneManager.openConfirmationAlert("Success", "Material added successfully");
+            Integer oldQuantity = MaterialService.getMaterialByName(materialConstructionFormController.materialComboBox.getValue()).getQuantity();
+            Integer newQuantity = oldQuantity - Integer.parseInt(materialConstructionFormController.quantity.getText());
+            Material material =  MaterialService.getMaterialByName(materialConstructionFormController.materialComboBox.getValue());
+            material.setQuantity(newQuantity);
+            MaterialService.update(material);
                 SceneManager.closeWindow(save);
             }
         } catch (Exception e){
@@ -54,9 +78,14 @@ public class AddMaterialConstructionModalController {
 
     public void getReady(){
         System.out.println("get ready");
+        List<String> names = MaterialService.getAllMaterialsNames();
+        materialConstructionFormController.materialComboBox.setItems(FXCollections.observableArrayList(names));
+        materialConstructionFormController.materialComboBox.setValue(names.get(0));
     }
 
     public void setConstruction(Construction construction) {
+        constructionID = construction.getId();
+        System.out.printf("Construction ID: %d\n", constructionID);
         materialConstructionFormController.constructionID.setText(String.valueOf(construction.getId()));
         materialConstructionFormController.constructionID.setDisable(true);
 
